aae3e337f2a222e4ebe84cd0b796d6ef94f7e023
averlbacher2 <aerlbacher2@huskers.unl.edu>
2025-09-12 13:42:39 -0500

    Fixed App.jsx

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -71,36 +71,8 @@ function App() {
           <section id="contact">
             <h2 className="section-heading">Contact</h2>
           </section>
-        </div> {/* End of .container */}
-      </div> {/* End of .main-content-wrapper */}
-          </div>
-        </section>
-        <section id="experience">
-          <h2>Experience</h2>
-          <p>
-            I am a current student at University of Nebraska-Lincoln, pursuing a
-            double major in Computer Science and Business Management with an
-            emphasis in Entrepreneurship.
-          </p>
-          {/* Use your React component for each work experience */}
-          <ExperiencesContainer experiences={experienceData} />
-          {/* Add more ExperienceCard components here */}
-        </section>
-        <section id="projects">
-          <h2>Projects</h2>
-          <ProjectsContainer projects={projectData} />
-        </section>
-        <section id="skills">
-          <h2>Skills</h2>
-          <p>
-            Languages: Java, Python, Basic JavaScript, HTML, CSS Frameworks:
-            React, Vite, Playwright Tools: Git, GitHub, Visual Studio Code, Figma
-          </p>
-        </section>
-        <section id="contact">
-          <h2>Contact</h2>
-        </section>
-      </div>
+        </div> 
+      </div> 
       <footer>
         {/* You can add footer content here, like contact information or copyright */}
       </footer>
